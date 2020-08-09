54df7bbc32086df00bcfe8e94720c6d850444862
kiraarghy <karaestubbs@gmail.com>
2020-08-09 14:22:44 +0100

    fix

--- build.js
+++ build.js
@@ -1,9 +1,19 @@
-const express = require('express');
+const express = require("express");
 
-const { startGraphqlServer, stopGraphqlServer, readRoutes } = require('junglejs');
+const {
+  startGraphqlServer,
+  stopGraphqlServer,
+  readRoutes,
+} = require("junglejs");
 
-const jungleConfig = require('./jungle.config');
+const jungleConfig = require("./jungle.config");
 
 const app = express();
 
-startGraphqlServer(jungleConfig, __dirname, () => readRoutes(jungleConfig, app, __dirname).then(() => stopGraphqlServer(() => null)));
+getJungleConfig().then((jungleConfig) =>
+  startGraphqlServer(jungleConfig, __dirname, () =>
+    readRoutes(jungleConfig, app, __dirname).then(() =>
+      stopGraphqlServer(() => null)
+    )
+  )
+);
